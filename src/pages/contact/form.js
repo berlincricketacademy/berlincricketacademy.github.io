2d145519bf4a843e62cc80c51c1815609da728f6
Princiya <princiya.marina@gmail.com>
2021-01-25 19:51:07 +0100

    updated max-width for contact form message

--- src/pages/contact/form.js
+++ src/pages/contact/form.js
@@ -37,81 +37,86 @@ export default class Index extends React.Component {
     return (
       <section className="section" style={{width: '80%'}}>
         <div className="container">
-          <div className="content">
-            <p>
-              Due to overwhelming response, we are currently disabling our contact form for 24 hours till 26.01.2021 12:00 CET. Meanwhile please contact us directly at contact@berlincricketacademy.com.
-            </p>
-            {/* <form
-              name="contact"
-              method="post"
-              action="/contact/thanks/"
-              data-netlify="true"
-              data-netlify-honeypot="bot-field"
-              onSubmit={this.handleSubmit}
-            >
-              The `form-name` hidden field is required to support form submissions without JavaScript
-              <input type="hidden" name="form-name" value="contact" />
-              <div hidden>
-                <label>
-                  Don’t fill this out:{' '}
-                  <input name="bot-field" onChange={this.handleChange} />
-                </label>
-              </div>
-              <div className="field">
-                <label className="label" htmlFor={'name'}>
-                  Name
-                </label>
-                <div className="control">
-                  <input
-                    className="input"
-                    type={'text'}
-                    name={'name'}
-                    onChange={this.handleChange}
-                    id={'name'}
-                    required={true}
-                    placeholder="Name"
-                  />
-                </div>
-              </div>
-              <div className="field">
-                <label className="label" htmlFor={'email'}>
-                  Email
-                </label>
-                <div className="control">
-                  <input
-                    className="input"
-                    type={'email'}
-                    name={'email'}
-                    onChange={this.handleChange}
-                    id={'email'}
-                    required={true}
-                    placeholder="Email Address"
-                  />
-                </div>
-              </div>
-              <div className="field">
-                <label className="label" htmlFor={'message'}>
-                  Message
-                </label>
-                <div className="control">
-                  <textarea
-                    className="textarea"
-                    name={'message'}
-                    onChange={this.handleChange}
-                    id={'message'}
-                    required={true}
-                  />
-                </div>
-              </div>
-              <div className="field">
-                <button className="button is-rounded bca-button-primary" type="submit">
-                  Submit
-                </button>
-              </div>
-            </form> */}
-          </div>
+          <p style={{maxWidth: '300px'}}>
+            Due to overwhelming response, we are currently disabling our contact form for 24 hours till 26.01.2021 12:00 CET. Meanwhile please contact us directly at <strong>contact@berlincricketacademy.com</strong>.
+            {/* Due to overwhelming response */}
+          </p>
         </div>
       </section>
+      // <section className="section" style={{width: '80%'}}>
+      //   <div className="container">
+      //     <div className="content">
+      //       <form
+      //         name="contact"
+      //         method="post"
+      //         action="/contact/thanks/"
+      //         data-netlify="true"
+      //         data-netlify-honeypot="bot-field"
+      //         onSubmit={this.handleSubmit}
+      //       >
+      //         {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+      //         <input type="hidden" name="form-name" value="contact" />
+      //         <div hidden>
+      //           <label>
+      //             Don’t fill this out:{' '}
+      //             <input name="bot-field" onChange={this.handleChange} />
+      //           </label>
+      //         </div>
+      //         <div className="field">
+      //           <label className="label" htmlFor={'name'}>
+      //             Name
+      //           </label>
+      //           <div className="control">
+      //             <input
+      //               className="input"
+      //               type={'text'}
+      //               name={'name'}
+      //               onChange={this.handleChange}
+      //               id={'name'}
+      //               required={true}
+      //               placeholder="Name"
+      //             />
+      //           </div>
+      //         </div>
+      //         <div className="field">
+      //           <label className="label" htmlFor={'email'}>
+      //             Email
+      //           </label>
+      //           <div className="control">
+      //             <input
+      //               className="input"
+      //               type={'email'}
+      //               name={'email'}
+      //               onChange={this.handleChange}
+      //               id={'email'}
+      //               required={true}
+      //               placeholder="Email Address"
+      //             />
+      //           </div>
+      //         </div>
+      //         <div className="field">
+      //           <label className="label" htmlFor={'message'}>
+      //             Message
+      //           </label>
+      //           <div className="control">
+      //             <textarea
+      //               className="textarea"
+      //               name={'message'}
+      //               onChange={this.handleChange}
+      //               id={'message'}
+      //               required={true}
+      //             />
+      //           </div>
+      //         </div>
+      //         <div className="field">
+      //           <button className="button is-rounded bca-button-primary" type="submit">
+      //             Submit
+      //           </button>
+      //         </div>
+      //       </form>
+      //     </div>
+      //   </div>
+      // </section>
     );
   }
 }
